c20fdf5be8cd549e4ca0778580f086c8961c458b
Faserly App <85167495+faserly@users.noreply.github.com>
2023-11-01 16:32:27 +0300

    Create the main dashboard elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import Image from 'next/image'
+import Header from '../components/Header';
+import TopCards from '../components/TopCards';
+import BarChart from '../components/BarChart';
+import RecentOrders from '../components/RecentOrders';
 
 export default function Home() {
   return (
-    <main></main>
+    <main className='bg-gray-100 min-h-screen'>
+      <Header />
+      <TopCards />
+      <div>
+        <BarChart />
+        <RecentOrders />
+      </div>
+    </main>
   )
 }
