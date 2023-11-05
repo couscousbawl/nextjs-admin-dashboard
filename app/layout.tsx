14bf2337c913cc8a6f62e325c4d372b8caa83fdd
Faserly App <85167495+faserly@users.noreply.github.com>
2023-11-05 13:38:43 +0300

    implement orders page

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className='bg-gray-100 min-h-screen'>
         <Sidebar>{children}</Sidebar>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import RecentOrders from '../components/RecentOrders';
 
 export default function Home() {
   return (
-    <main className='bg-gray-100 min-h-screen'>
-      <Header />
+    <main>
+      <Header PageName='Dashboard'/>
       <TopCards />
       <div className='p-4 grid md:grid-cols-3 grid-cols-1 gap-4'>
         <BarChart />
